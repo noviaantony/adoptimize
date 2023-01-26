79d68cedf07a6f42fa349b9220d43367504c46e0
chen zhao xing <chenzhaoxing.98@gmail.com>
2023-01-27 02:15:26 +0800

    feat - Added user entity

---
+++ AdoptEasy/src/test/java/com/example/AdoptEasy/AdoptEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdoptEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
