bf8db858e2b3a5238d144717ea0ba86c4d0105db
newbie shines <chenzhaoxing.98@gmail.com>
2023-01-26 22:29:50 +0800

    build - spring initialization

---
+++ AdoptEasy/src/main/java/com/example/AdoptEasy/AdoptEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdoptEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptEasyApplication.class, args);
+	}
+
+}
