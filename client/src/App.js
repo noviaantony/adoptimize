341bbce522db392bd2a0c63295539e6fdf8d89d5
Novia Antony <noviaantony2001@gmail.com>
2023-01-31 23:26:12 +0800

    feat: Create hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 
+
 // import Sidebar from './components/navigation/Sidebar'
 import Navbar from './components/navigation/Navbar'
+import Navbar2 from './components/navigation/Navbar2'
 import Footer from "./components/footer/Footer";
 
 import Home from "./screens/Home";
@@ -11,12 +13,14 @@ import Prescreening from './screens/Prescreening';
 import AdoptionContract from './screens/AdoptionContract';
 import AdoptionManagement from './screens/AdoptionManagement'
 import AboutUs from './screens/AboutUs';
+import Landing from './screens/Landing';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      {/* <Navbar2/> */}
       {/* <Home/> */}
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
@@ -25,6 +29,7 @@ function App() {
         <Route path="/AdoptionContract" element={<AdoptionContract />} />
         <Route path="/AdoptionManagement" element={<AdoptionManagement />} />
         <Route path="/AboutUs" element ={<AboutUs/>} />
+        <Route path = "/" element = {<Landing/>}/>
 
       </Routes> 
       {/* <Footer/> */}
