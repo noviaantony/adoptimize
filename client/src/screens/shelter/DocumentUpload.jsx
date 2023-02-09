625c8197b05f19e8b327ef2506abe0656c1cf791
Novia Antony <noviaantony2001@gmail.com>
2023-02-09 13:29:01 +0800

    fix: Pdf viewer debugging

--- client/src/screens/shelter/DocumentUpload.jsx
+++ client/src/screens/shelter/DocumentUpload.jsx
@@ -278,7 +278,7 @@ const DocumentUpload = () => {
   
         {/* <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white mr-3 text-3xl" for="file_input">Upload file</label> */}
 <input class="w-62 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-3xl" id="file_input" type="file" onChange={handleChange} />
-        {/* <button type="submit" className='btn btn-success'>View PDF</button> */}
+        <button type="submit" className='btn btn-success'>View PDF</button>
       </form>
 
       <button class="bg-gray-700 hover:bg-gray-500 focus:bg-white focus:text-gray-500 transition duration-300 text-white font-bold py-2 px-4 rounded-full font-nunito text-sm px-10 py-2 text-xl mt-6"  onClick={handleClickOpen} >
@@ -286,14 +286,14 @@ const DocumentUpload = () => {
       </button>
 
       {/* <h2>View PDF</h2> */}
-      {/* <div className='pdf-container'>
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.2.146/build/pdf.worker.min.js">
+      <div className='pdf-container'>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
           {viewPdf && <>
             <Viewer fileUrl={viewPdf} plugins={[newplugin]}/>
           </>}
           {!viewPdf && <>No PDF</>}
         </Worker>
-      </div> */}
+      </div>
     </div>
 
 
