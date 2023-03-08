d1e05f6a9b84429ad5891355ec45e359f993859b
Novia Antony <noviaantony2001@gmail.com>
2023-03-08 14:14:35 +0800

    feat: Create from builder for pre-screening questions

--- client2/src/components/routes/AppRoutes.jsx
+++ client2/src/components/routes/AppRoutes.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Adoptions from '../../screens/Adoptions'
 import AllPets from '../../screens/AllPets'
+import Prescreening from '../../screens/Prescreening';
 
 const AppRoutes = () => {
   return (
-    
-      <Routes>
-        <Route path="/" element={<AllPets />}/>
-        <Route path="/Adoptions" element={<Adoptions />}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<AllPets />} />
+      <Route path="/Adoptions" element={<Adoptions />} />
+      <Route path="/PreScreeningQuestions" element={<Prescreening />} />
+    </Routes>
   );
 }
 
--- client2/src/screens/Prescreening.jsx
+++ client2/src/screens/Prescreening.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import {
+  Avatar,
+  Rate,
+  Space,
+  Table,
+  Typography,
+  Tag,
+  Dropdown,
+  message,
+  Button,
+  Form,
+} from "antd";
+import { HolderOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import "./../App.css";
+import { StyleProvider } from "@ant-design/cssinjs";
+import SurveyCreatorRenderComponent from "../components/prescreening/SurveyCreatorComponent";
+
 
 const Prescreening = () => {
-  return <div>Pre-screening</div>;
+  return (
+    <Space direction="vertical table">
+      <Typography.Title
+        className="font-nunito font-bold font-6x mt-5"
+      >
+        Pre-Screening Questions
+      </Typography.Title>
+      <SurveyCreatorRenderComponent/>
+    </Space>
+  );
 };
 
 export default Prescreening;
